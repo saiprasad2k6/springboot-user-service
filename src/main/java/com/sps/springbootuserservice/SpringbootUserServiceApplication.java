944e6acfe13503f58e8ba18d9a9f9ab8f3d9ca5d
saiprasad2k6 <saiprasad2k6@gmail.com>
2024-02-13 22:28:14 +0530

    Initial Revision

---
+++ src/main/java/com/sps/springbootuserservice/SpringbootUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sps.springbootuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sps/springbootuserservice/SpringbootUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sps.springbootuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
