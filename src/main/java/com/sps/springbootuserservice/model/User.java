2d4ea0f977731101500ad10ae70fd0c95a55dfe1
saiprasad2k6 <saiprasad2k6@gmail.com>
2025-01-21 22:47:28 +0530

    Fixing Issues

--- src/main/java/com/sps/springbootuserservice/model/User.java
+++ src/main/java/com/sps/springbootuserservice/model/User.java
@@ -20,6 +20,6 @@ public class User extends BaseModel {
     private String email;
     private String password;
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "roles_id"))
     private Set<Role> roles = new HashSet<>();
 }
