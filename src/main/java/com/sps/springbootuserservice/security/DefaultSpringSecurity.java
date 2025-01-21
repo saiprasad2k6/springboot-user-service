574d15889b9a75053242c0156d75380541052dd4
saiprasad2k6 <saiprasad2k6@gmail.com>
2025-01-22 00:13:05 +0530

    Disabling Default Spring Filters

--- src/main/java/com/sps/springbootuserservice/security/DefaultSpringSecurity.java
+++ src/main/java/com/sps/springbootuserservice/security/DefaultSpringSecurity.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class DefaultSpringSecurity {
 
-    @Bean
+    //@Bean
     public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()  // Disables CSRF protection
+                //.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/logout").denyAll())
+                //.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/*").authenticated())
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
         return http.build();
     }
 
