ef60dafd2f2837886f92569090b904294f4a8960
saiprasad2k6 <saiprasad2k6@gmail.com>
2025-01-21 22:47:06 +0530

    Initial Revision

---
+++ src/main/java/com/sps/springbootuserservice/security/DefaultSpringSecurity.java
@@ -0,0 +1,19 @@
+package com.sps.springbootuserservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DefaultSpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()  // Disables CSRF protection
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
+    }
+
+}
