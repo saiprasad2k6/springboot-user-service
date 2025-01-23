f3ca8948fa9e2974a101c8214ef0f74c5dc602e9
saiprasad2k6 <saiprasad2k6@gmail.com>
2025-01-23 15:23:34 +0530

    Initial Revision

---
+++ src/main/java/com/sps/springbootuserservice/security/repositories/AuthorizationConsentRepository.java
@@ -0,0 +1,14 @@
+package com.sps.springbootuserservice.security.repositories;
+
+
+import com.sps.springbootuserservice.security.models.AuthorizationConsent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
---
+++ src/main/java/com/sps/springbootuserservice/security/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.sps.springbootuserservice.security.repositories;
+
+
+import com.sps.springbootuserservice.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
